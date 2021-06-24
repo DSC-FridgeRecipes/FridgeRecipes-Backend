1fc14234d34f8e1a08377c69b37f6cf163152bf8
SoYi Jeon <soyijeon47@gmail.com>
2021-06-25 01:58:14 +0900

    🌱  Update graphql field types

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -3,44 +3,45 @@ const { gql } = require('apollo-server');
 const typeDefs = gql`
 
   type Recipe {
-    title: String
-    ingredientNameList: [String]
-    ingredientAmountList: [String]
-    content: String
+    title: String!
+    ingredientNameList: [String]!
+    ingredientAmountList: [String]!
+    content: String!
   }
 
   type User {
-    email: String,,
-    recipes: [ID],
-    ingredients: [String],
+    email: String!
+    recipes: [ID]!
+    ingredients: [String]!
   }
 
   type Query {
     ### Recipe ###
-    getAllMyRecipes(userId: ID): [Recipe]
+    getAllMyRecipes(userId: ID!): [Recipe]!
   }
 
   type Mutation {
     ### User ###
-    signup(email: String): String
-    login(email: String): String
+    signup(email: String!): String!
+    login(email: String!): String!
 
     ### Recipe ###
     createRecipe(
-        title: String,
-        ingredientNameList: [String],
-        ingredientAmountList: [String],
-        content: String
-        ): ID
+        createUser: ID!
+        title: String!
+        ingredientNameList: [String]!
+        ingredientAmountList: [String]!
+        content: String!
+        ): ID!
 
     ### User - ingredients ###
-    addMyIngredient(userId: ID, ingredient: String): String
-    deleteMyIngredient(userId: ID, ingredient: String): String
-    deleteAllMyIngredient(userId: ID): String
+    addMyIngredient(userId: ID!, ingredient: String!): String!
+    deleteMyIngredient(userId: ID!, ingredient: String!): String!
+    deleteAllMyIngredient(userId: ID!): String!
 
     ### User - recipes ###
-    addMyRecipe(userId: ID, recipeId: ID): String
-    deleteMyRecipe(userId: ID, recipeId: ID): String
+    addMyRecipe(userId: ID!, recipeId: ID!): String!
+    deleteMyRecipe(userId: ID!, recipeId: ID!): String!
   }
 `;
 
--- models/Recipe.js
+++ models/Recipe.js
@@ -1,7 +1,6 @@
 const { model, Schema } = require('mongoose');
 
 const Recipe = new Schema({
-    // createUserId: { type: Schema.Types.ObjectId, ref: 'User', required: true, index: true },
     title: String,
     ingredientNameList: [String],
     ingredientAmountList: [String],
